f5702aee8d8d61b4ee19a9bf4396fe87a8141707
AayushChitransh11 <aayush11.ac@gmail.com>
2025-03-12 00:00:01 -0400

    sorting

--- L10_Sorting/BubbleSort.java
+++ L10_Sorting/BubbleSort.java
@@ -8,7 +8,7 @@ public class BubbleSort {
     }
 
     public static int[] bubbleSort(int[] A,int n){
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n-1;i++){
             boolean flag=false;
             for(int j=0;j<n-i-1;j++){
                 if(A[j]>A[j+1]){
